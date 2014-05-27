e83265b50a2f06632478034f98ff1dbd64098a6e
Stephane <lossendae@gmail.com>
2014-05-28 00:29:42 +0200

    Added Validator service + User validation

---
+++ src/services/Validators/User.php
@@ -0,0 +1,21 @@
+<?php namespace Lossendae\PreviouslyOn\Services\Validators;
+
+class User extends Validator
+{
+    public static $rules = array(
+        'create' => array(
+            'email' => array('required', 'email', 'unique:users'),
+            'pwd' => array('required', 'min:6', 'max:18'),
+            'username' => array('required', 'min:3', 'max:16', 'unique:users'),
+        ),
+        'update' => array(
+            'email' => array('required', 'email'),
+            'pwd' => array('min:6', 'max:18'),
+            'username' => array('required', 'min:3', 'max:16'),
+        ),
+        'login' => array(
+            'username' => array('required'),
+            'pwd' => array('required', 'min:6', 'max:18'),
+        ),
+    );
+}
---
+++ src/services/Validators/Validator.php
@@ -0,0 +1,79 @@
+<?php namespace Lossendae\PreviouslyOn\Services\Validators;
+ 
+abstract class Validator {
+
+    /**
+     * @var array
+     */
+    protected $attributes;
+
+    /**
+     * @var array
+     */
+    protected $errors;
+
+    /**
+     * @var null
+     */
+    protected $level;
+
+    /**
+     * @var array
+     */
+    public static $rules = array();
+
+    /**
+     * @param null $data
+     * @param null $level
+     */
+    public function __construct($data = null, $level = null)
+    {
+        $this->attributes = $data ?: \Input::all();
+        $this->level = $level;
+    }
+
+    /**
+     * @return bool
+     */
+    public function passes()
+    {
+        $rules = array();
+        if($this->level !== null)
+        {
+            $rules = static::$rules[$this->level];
+        }
+        else
+        {
+            $rules = static::$rules;
+
+        }
+        
+        $messages = array();
+        if(is_array(trans('admin::validation')))
+        {
+            $messages = trans('admin::validation');
+        }
+
+        $validation = \Validator::make($this->attributes, $rules, $messages);
+
+        if($validation->passes())
+        {
+            return true;
+        }
+
+        $this->errors = $validation->messages()->getMessages();
+
+        return false;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getErrors()
+    {
+        return $this->errors;
+    }
+
+}
+
+
