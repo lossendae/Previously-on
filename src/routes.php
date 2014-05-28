7e4d27aa6c1300a65324063f383109ceb547da1a
Stephane <lossendae@gmail.com>
2014-05-28 10:33:13 +0200

    Added auth routes

--- src/routes.php
+++ src/routes.php
@@ -2,8 +2,6 @@
 
 $base = 'Lossendae\PreviouslyOn\Controllers\\';
 
-Route::get('/', $base . 'IndexPageController@index');
-
 Route::group(array('before' => 'xsrf', 'prefix' => 'api'), function () use($base)
 {
     Route::get('remote/search', $base . 'Api\SearchController@get');
@@ -15,5 +13,33 @@ Route::group(array('before' => 'xsrf', 'prefix' => 'api'), function () use($base
     Route::delete('manage/{id}', $base . 'ManageController@removeTvShow');
 });
 
-Route::get('/{all}', $base . 'IndexPageController@index')
-     ->where('all', '.*');
+Route::group(array('before' => 'xsrf'), function () use($base)
+{
+    Route::get('auth/logout', $base . 'AuthController@logout');
+});
+
+Route::get('auth/token', function ()
+{
+    return array('_token' => csrf_token());
+});
+
+Route::get('auth/check', function ()
+{
+        return array('logged' => Auth::check());
+});
+
+Route::get('auth/session', function ()
+{
+    return ['user' => Auth::check() ? Auth::user()->toArray() : false];
+});
+
+Route::post('auth/login', $base . 'AuthController@login');
+
+Route::group(array('prefix' => Config::get('previously-on::app.url_prefix')), function () use ($base)
+{
+    Route::get('/', $base . 'IndexPageController@index');
+
+    // Lastly all the remaining url access will redirect to the index
+    Route::get('/{all}', $base . 'IndexPageController@index')
+         ->where('all', '.*');
+});
