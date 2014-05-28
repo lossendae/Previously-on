117e93de4450165694fc7590ead0dfa000a4f69f
Stephane <lossendae@gmail.com>
2014-05-28 10:32:55 +0200

    Adeed crsf toakn filter

--- src/filters.php
+++ src/filters.php
@@ -2,12 +2,14 @@
 
 Route::filter('xsrf', function()
 {
-//    $sessionToken = Session::token();
-//    if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-//        if ($sessionToken != Request::header('X-XSRF-TOKEN'))
-//        {
-//            $response  = ['error' => ['message' => 'Token Mismatch']];
-//            return Response::json($response, 401);
-//        }
-//    }
+    $sessionToken = Session::token();
+    if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+        if ($sessionToken != Request::header('X-XSRF-TOKEN'))
+        {
+            $response  = array('error' => array(
+                'message' => 'Token Mismatch'
+            ));
+            return Response::json($response, 401);
+        }
+    }
 });
