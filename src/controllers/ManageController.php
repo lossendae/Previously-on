7077800d08b541a323195c6db1b0a9f8e170ae7b
Stephane <lossendae@gmail.com>
2014-05-31 19:03:03 +0200

    Ongoing refactor for more streamline controllers

--- src/controllers/ManageController.php
+++ src/controllers/ManageController.php
@@ -7,82 +7,42 @@
 * For the full copyright and license information, please view the LICENSE
 * file that was distributed with this source code.
 */
-
 namespace Lossendae\PreviouslyOn\Controllers;
 
-use Config, DB;
-use Lossendae\PreviouslyOn\Models\TvShow;
-use Lossendae\PreviouslyOn\Models\Episode;
-use Lossendae\PreviouslyOn\Models\User;
-
-class ManageController extends BaseController
+/**
+ * Class AssignedTvShowController
+ *
+ * @package Lossendae\PreviouslyOn\Controllers
+ */
+class AssignedTvShowController extends BaseController
 {
+    /**
+     * @var array
+     */
     protected $seasons = array();
 
     /**
-     * Get the list of the TV shows cached in the db
+     * Get the list of the TV shows
      *
      * @return array
      */
-    public function query()
+    public function getList()
     {
         return $this->app['tvshow.service']->getList($this->user);
     }
 
     /**
-     * Retrieve all a show and all its episodes
+     * Get all a show and attached episodes
      *
      * @param int $id the TV Show id
      * @return array
      */
-    public function listSeasons($id)
+    public function manage($id)
     {
-        $data = $row = [];
+        $result = $this->app['tvshow.service']->getOne($id, $this->user);
+        $result = array_merge($result, $this->app['episode.service']->getList($id, $this->user));
 
-        $query  = TvShow::select('tv_shows.*')
-                        ->oneWithRemaining($id, $this->user->id);
-        $result = $query->first();
-
-        $data['serie'] = $result->toArray();
-
-        $episodes = Episode::select('episodes.*')
-                           ->withStatus($id, $this->user->id)
-                           ->get();
-
-        foreach($episodes as $episode)
-        {
-            $row = $episode->toArray();
-            $this->processRow($row);
-        }
-        $data['seasons'] = $this->seasons;
-
-        $response['success'] = true;
-        $response['total']   = TvShow::count();
-        $response['data']    = $data;
-
-        return $response;
-    }
-
-    /**
-     * @param array $episode
-     */
-    protected function processRow($episode)
-    {
-        $season = $episode['season_number'];
-        if($episode['episode_number'] < 10)
-        {
-            $episode['episode_number'] = str_pad($episode['episode_number'], 2, 0, STR_PAD_LEFT);
-        }
-        if($episode['season_number'] < 10)
-        {
-            $episode['season_number'] = str_pad($episode['season_number'], 2, 0, STR_PAD_LEFT);
-        }
-        $episode['air_date'] = !is_null($episode['first_aired']) ? date('d M Y', strtotime($episode['first_aired'])) : 'TBA';
-        $episode['disabled'] = is_null($episode['first_aired']) || strtotime($episode['first_aired']) > strtotime('now') ? 'disabled' : '';
-
-        $episode['status'] = $episode['status'] ? true : false;
-
-        $this->seasons[$season]['episodes'][] = $episode;
+        return $result;
     }
 
     /**
@@ -91,7 +51,7 @@ class ManageController extends BaseController
      * @param int $id
      * @return array
      */
-    public function removeTvShow($id)
+    public function remove($id)
     {
         $response = array('success' => false);
         $tvShow   = TvShow::find($id);
